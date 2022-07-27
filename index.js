b4ec55e3dfd7bf9d5314112ceda447c3b6a3522f
abrar-byte <usamahhafidz99@gmail.com>
2022-07-27 22:18:10 +0700

    finis32345

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-import User from './models/UserModel';
 
 const app = express();
 app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
 
-app.get("/users", function (req, res) {
-  try {
-    const response = await User.findAll();
-    res.status(200).json(response);
-} catch (error) {
-    console.log(error.message);
-}
-})
+
 app.use(cors());
 app.use(express.json());
-// app.use(UserRoute);
+app.use(UserRoute);
 
 app.listen(process.env.PORT || 5000, ()=> console.log('Server up and running...'));
