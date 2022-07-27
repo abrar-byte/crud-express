17a6090c01142298a88707a47d8de82db6f96e2b
abrar-byte <usamahhafidz99@gmail.com>
2022-07-27 22:04:30 +0700

    finis323

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const app = express();
 app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
+
+app.get("/users", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 app.use(cors());
 app.use(express.json());
-app.use(UserRoute);
+// app.use(UserRoute);
 
 app.listen(process.env.PORT || 5000, ()=> console.log('Server up and running...'));
