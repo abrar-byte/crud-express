8e511b7a0953d8270effb252b73ff12b9fb419fa
abrar-byte <usamahhafidz99@gmail.com>
2022-07-27 21:59:26 +0700

    finish2

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import UserRoute from "./routes/UserRoute.js";
 import { createUser, deleteUser, getUserById, getUsers, updateUser } from "./controllers/UserController.js";
 
 const app = express();
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+// app.get("/", function (req, res) {
+//   res.send("<h1>Hello World!</h1>")
+// })
 app.use(cors());
 app.use(express.json());
 // app.use(UserRoute);
