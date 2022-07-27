b462e376bfdca5a7769b91bd7d1df5fe2b88fb60
abrar-byte <usamahhafidz99@gmail.com>
2022-07-28 05:26:41 +0700

    new2

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import express from "express";
 import cors from "cors";
-import UserRoute from "./routes/UserRoute.js";
+// import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 app.get("/", function (req, res) {
   res.send("<h1>Hello World!</h1>")
 })
-
-
 app.use(cors());
 app.use(express.json());
 // app.use(UserRoute);
