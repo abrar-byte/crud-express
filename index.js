1864d724a2e10cdc34ace7691eea948d844610d1
abrar-byte <usamahhafidz99@gmail.com>
2022-07-27 21:58:02 +0700

    finish

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
+import { createUser, deleteUser, getUserById, getUsers, updateUser } from "./controllers/UserController.js";
 
 const app = express();
 app.get("/", function (req, res) {
@@ -8,6 +9,12 @@ app.get("/", function (req, res) {
 })
 app.use(cors());
 app.use(express.json());
-app.use(UserRoute);
+// app.use(UserRoute);
+app.get('/users', getUsers);
+app.get('/users/:id', getUserById);
+app.post('/users', createUser);
+app.patch('/users/:id', updateUser);
+app.delete('/users/:id', deleteUser);
+
 
 app.listen(process.env.PORT || 5000, ()=> console.log('Server up and running...'));
