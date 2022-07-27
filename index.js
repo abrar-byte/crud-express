f525abd31becd39bef7d635f381cbf854fc07817
abrar-byte <usamahhafidz99@gmail.com>
2022-07-27 21:53:54 +0700

    wow

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
