a23b15841cf0db43c4cdccc0679cd15482765bde
abrar-byte <usamahhafidz99@gmail.com>
2022-07-27 22:02:01 +0700

    finis32

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-import { createUser, deleteUser, getUserById, getUsers, updateUser } from "./controllers/UserController.js";
 
 const app = express();
-// app.get("/", function (req, res) {
-//   res.send("<h1>Hello World!</h1>")
-// })
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 app.use(cors());
 app.use(express.json());
-// app.use(UserRoute);
-app.get('/users', getUsers);
-app.get('/users/:id', getUserById);
-app.post('/users', createUser);
-app.patch('/users/:id', updateUser);
-app.delete('/users/:id', deleteUser);
-
+app.use(UserRoute);
 
 app.listen(process.env.PORT || 5000, ()=> console.log('Server up and running...'));
