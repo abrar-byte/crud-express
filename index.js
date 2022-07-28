c9c0c4d9a190f97e0fa34bc9cfe4bfabca3986f4
abrar-byte <usamahhafidz99@gmail.com>
2022-07-28 09:19:55 +0700

    test52

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-// import UserRoute from "./routes/UserRoute.js";
+import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
 app.get("/", function (req, res) {
@@ -10,6 +10,6 @@ app.get("/", function (req, res) {
 
 app.use(cors());
 app.use(express.json());
-// app.use(UserRoute);
+app.use(UserRoute);
 
 app.listen(process.env.PORT || 5000, ()=> console.log('Server up and running...'));
