0a80e5bccbd9d87430a82a7628903d03fe1bddcf
abrar-byte <usamahhafidz99@gmail.com>
2022-07-28 08:48:51 +0700

    cek

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(process.env.PORT || 3002, ()=> console.log('Server up and running...'));
+app.listen(process.env.PORT || 5000, ()=> console.log('Server up and running...'));
