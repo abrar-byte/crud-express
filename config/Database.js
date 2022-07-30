bc52fe414d5c5f59b57522666919a571dddd68e9
abrar-byte <usamahhafidz99@gmail.com>
2022-07-30 23:25:27 +0700

    third3

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,23 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('testcrud','admin','mysql12345',{
-    host: 'hobin-new.cu7kqg4havxg.us-east-1.rds.amazonaws.com',
+// const db = new Sequelize('testcrud','admin','mysql12345',{
+//     host: 'hobin-new.cu7kqg4havxg.us-east-1.rds.amazonaws.com',
+//     dialect: 'mysql'
+// });
+
+// const db = new Sequelize('hobindatabase','admin','mysql123456',{
+//     host: 'database-hobin.cu7kqg4havxg.us-east-1.rds.amazonaws.com',
+//     dialect: 'mysql'
+// });
+
+// const db = new Sequelize('crud','abrar','Abcd#1234',{
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+
+const db = new Sequelize('hobintest','admin','admin123',{
+    host: 'databasehobin.cu7kqg4havxg.us-east-1.rds.amazonaws.com',
     dialect: 'mysql'
 });
-
 export default db;
